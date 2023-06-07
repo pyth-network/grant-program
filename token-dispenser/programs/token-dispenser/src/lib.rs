c45b82c7940e5facacc35867ea6b833d0a26ee7a
guibescos <59208140+guibescos@users.noreply.github.com>
2023-06-07 14:02:23 +0100

    [token-dispenser] implement inclusion verification (#4)
    * Rename proof
    
    * Clneaup
    
    * Remove traits
    
    * Add inclusion proof from pyth sdk
    
    * Verify
    
    * Fix comment
    
    * Cleanup
    
    * Fix typo
    
    * Cleanup
    
    * Cleanup

--- token-dispenser/programs/token-dispenser/src/lib.rs
+++ token-dispenser/programs/token-dispenser/src/lib.rs
@@ -1,4 +1,9 @@
 use anchor_lang::prelude::*;
+use pythnet_sdk::accumulators::merkle::{
+    MerklePath,
+    MerkleRoot,
+};
+use pythnet_sdk::hashers::keccak256::Keccak256;
 use std::collections::HashSet;
 use std::mem::{
     self,
@@ -11,7 +16,6 @@ const CONFIG_SEED: &[u8] = b"config";
 #[program]
 pub mod token_dispenser {
     use super::*;
-    use anchor_lang::solana_program::keccak;
 
     /// This can only be called once and should be called right after the program is deployed.
     pub fn initialize(ctx: Context<Initialize>, target_config: Config) -> Result<()> {
@@ -21,11 +25,12 @@ pub mod token_dispenser {
 
     /**
      * Claim a claimant's tokens. This instructions needs to enforce :
-     * - The dispenser guard has signed the transaction
-     * - The claimant is not claiming tokens for more than one ecosystem
+     * - The dispenser guard has signed the transaction - DONE
+     * - The claimant is claiming no more than once per ecosystem - DONE
      * - The claimant has provided a valid proof of identity (is the owner of the wallet
      *   entitled to the tokens)
-     * - The claimant has provided a valid proof of inclusion (this confirm that the claimant
+     * - The claimant has provided a valid proof of inclusion (this confirm that the claimant --
+     *   DONE
      * - The claimant has not already claimed tokens
      */
     pub fn claim(ctx: Context<Claim>, claim_certificates: Vec<ClaimCertificate>) -> Result<()> {
@@ -36,19 +41,18 @@ pub mod token_dispenser {
         // Check that the claimant is not claiming tokens for more than one ecosystem
         verify_one_identity_per_ecosystem(&claim_certificates)?;
 
+        let merkle_root = MerkleRoot::<Keccak256>::new(config.merkle_root);
+
         // TO DO : Actually check the proof of identity and the proof of inclusion
         for claim_certificate in &claim_certificates {
             // Each leaf of the tree is a hash of the serialized claim info
             // The identity is derived from the proof of identity (signature)
             // If the proof of identity does not correspond to a whitelisted identiy, the inclusion
             // verification will fail
-            let leaf: [u8; 32] =
-                keccak::hashv(&[get_claim(claim_certificate).try_to_vec()?.as_slice()]).0;
-            verify_inclusion(
-                &leaf,
-                &claim_certificate.proof_of_inclusion,
-                &config.merkle_root,
-            )?;
+            merkle_root.check(
+                MerklePath::<Keccak256>::new(claim_certificate.proof_of_inclusion.clone()),
+                get_claim(claim_certificate).try_to_vec()?.as_slice(),
+            );
             total_amount = total_amount
                 .checked_add(claim_certificate.amount)
                 .ok_or(ErrorCode::ArithmeticOverflow)?;
@@ -131,7 +135,7 @@ pub enum ProofOfIdentity {
     Cosmwasm,
 }
 
-pub fn get_claim(claim_certificate : &ClaimCertificate) -> ClaimInfo{
+pub fn get_claim(claim_certificate: &ClaimCertificate) -> ClaimInfo {
     ClaimInfo {
         identity: get_identity(&claim_certificate.proof_of_identity),
         amount:   claim_certificate.amount,
@@ -159,15 +163,6 @@ pub fn verify_one_identity_per_ecosystem(claim_certificates: &Vec<ClaimCertifica
     if hash_set.len() != claim_certificates.len() {
         return Err(ErrorCode::MoreThanOneIdentityPerEcosystem.into());
     }
-    Ok(())
-}
-
-pub fn verify_inclusion(
-    leaf: &[u8; 32],
-    merkle_proof: &[[u8; 32]],
-    merkle_root: &[u8; 32],
-) -> Result<()> {
-    // TO DO
     Ok(())
 }
 
