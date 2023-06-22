07d9b78c7a709732c1149a9aa0b65f6185a0688d
Guillermo Bescos <g.bescos@yahoo.com>
2023-06-22 15:11:50 +0100

    Checkpoint

--- token-dispenser/programs/token-dispenser/src/tests/test_verify_sig.rs
+++ token-dispenser/programs/token-dispenser/src/tests/test_verify_sig.rs
@@ -1,15 +1,7 @@
-// const ETH_SAMPLE_MESSAGE : String = "localhost:3000 wants you to sign in with your Ethereum account:\n" +
-// "0xf3f9225A2166861e745742509CED164183a626d7\n" +
-// "\n" +
-// "Sign In With Ethereum to prove you control this wallet.\n" +
-// "\n" +
-// "URI: http://localhost:3000\n" +
-// "Version: 1\n" +
-// "Chain ID: 1\n" +
-// "Nonce: hjCoOeDU55zTKLwOc\n" +
-// "Issued At: 2023-06-21T16:29:13.024Z";
+use libsecp256k1::RecoveryId;
 
-// const ETH_SAMPLE_SIGNATURE : String = "0x031bf6d550f95621494e7dcfc79cb42a6cce5aa59f5c343c674c3b6269839eb51558caceecaa8f3b4c344bb17d6c6b57dd5a34dc9d431c45ff2e75383959b6821c";
+const ETH_SAMPLE_MESSAGE : &str = "\x19Ethereum Signed Message:\n275localhost:3000 wants you to sign in with your Ethereum account:\n0xf3f9225A2166861e745742509CED164183a626d7\n\nSign In With Ethereum to prove you control this wallet.\n\nURI: http://localhost:3000\nVersion: 1\nChain ID: 1\nNonce: wIdVdFLtFSwM6Cfri\nIssued At: 2023-06-22T12:45:06.577Z";
+
 use {
     super::dispenser_simulator::DispenserSimulator,
     crate::tests::verify::verify_secp256k1_signature,
@@ -31,6 +23,26 @@ use {
 };
 
 const HELLO: &str = "hello";
+
+#[test]
+pub fn verify_sig_message() {
+    // let pubkey = hex::decode("f3f9225A2166861e745742509CED164183a626d7").unwrap();
+    println!("{:?}", ETH_SAMPLE_MESSAGE.as_bytes());
+    let hash = libsecp256k1::Message::parse(&Keccak256::hashv(&[ETH_SAMPLE_MESSAGE.as_bytes()]));
+    let signature = "dac0dfe99fb958f80aa0bda65b4fe3b02a7f4d07baa8395b5dad8585e69fe5d05d9a52c108d201a4465348b3fd8aecd7e56a9690c0ee584fd3b8d6cd7effb46d";
+    let signature_bytes = hex::decode(signature).expect("Decoding failed");
+    let mut signature_array: [u8; 64] = [0; 64];
+    signature_array.copy_from_slice(&signature_bytes[..]);
+    let signature_string =
+        libsecp256k1::Signature::parse_standard(&signature_array).expect("Decoding failed");
+
+    let recovery_id = RecoveryId::parse_rpc(0x1b).unwrap();
+
+    let eth = libsecp256k1::recover(&hash, &signature_string, &recovery_id).unwrap();
+
+    println!("{:?}", ETH_SAMPLE_MESSAGE.len());
+    println!("{:?}", hex::encode(construct_eth_pubkey(&eth)));
+}
 
 #[tokio::test]
 pub async fn verify_sig() {
