11eb5c823321c27b0356852bd0cf21d37e7fd337
Guillermo Bescos <g.bescos@yahoo.com>
2023-08-02 19:56:10 +0100

    Complete

--- token-dispenser/programs/token-dispenser/src/ecosystems/aptos.rs
+++ token-dispenser/programs/token-dispenser/src/ecosystems/aptos.rs
@@ -3,18 +3,14 @@ use {
     crate::ErrorCode,
     anchor_lang::{
         prelude::*,
-        solana_program::{
-            self,
-            hash,
-        },
+        solana_program::hash,
         AnchorDeserialize,
         AnchorSerialize,
     },
 };
 
-pub const APTOS_PREFIX: &str = "APTOS";
-pub const APTOS_MESSAGE_PREFIX: &str = "message: ";
-pub const APTOS_MESSAGE_NONCE: &str = "nonce: ";
+pub const APTOS_PREFIX: &[u8] = b"APTOS\nmessage: ";
+pub const APTOS_SUFFIX: &[u8] = b"\nnonce: ";
 pub const APTOS_SIGNATURE_SCHEME_ID: u8 = 0;
 
 
@@ -28,22 +24,9 @@ pub struct AptosMessage(Vec<u8>);
 
 impl AptosMessage {
     pub fn parse(data: &[u8]) -> Result<Self> {
-        let data_as_string = std::str::from_utf8(data)
-            .map_err(|_| ErrorCode::SignatureVerificationWrongMessageMetadata)?;
-        let mut iter = data_as_string.split("\n");
-
-        if let Some(prefix) = iter.next() {
-            if let Some(data) = iter.next() {
-                if let Some(nonce) = iter.next() {
-                    if iter.next().is_none()
-                        && prefix == APTOS_PREFIX
-                        && nonce == APTOS_MESSAGE_NONCE
-                    {
-                        if data.starts_with(APTOS_MESSAGE_PREFIX) {
-                            return Ok(Self(data[APTOS_MESSAGE_PREFIX.len()..].bytes().collect()));
-                        }
-                    }
-                }
+        if let Some(no_prefix) = data.strip_prefix(APTOS_PREFIX) {
+            if let Some(message) = no_prefix.strip_suffix(APTOS_SUFFIX) {
+                return Ok(AptosMessage(message.to_vec()));
             }
         }
         Err(ErrorCode::SignatureVerificationWrongMessageMetadata.into())
@@ -70,9 +53,9 @@ impl AptosMessage {
         Self(message.as_bytes().to_vec())
     }
     pub fn get_message_with_metadata(&self) -> Vec<u8> {
-        let mut message = format!("{}{}{}", APTOS_PREFIX, "\n", APTOS_MESSAGE_PREFIX).into_bytes();
+        let mut message = APTOS_PREFIX.to_vec();
         message.extend_from_slice(&self.0);
-        message.extend_from_slice(&format!("{}{}", "\n", APTOS_MESSAGE_NONCE).into_bytes());
+        message.extend_from_slice(&APTOS_SUFFIX);
         message.to_vec()
     }
 
