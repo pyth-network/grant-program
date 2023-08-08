0bdecdd8fda9173f8803266d7d2a63e5fcd49bbf
Guillermo Bescos <g.bescos@yahoo.com>
2023-08-08 19:43:21 +0100

    Remove unused file

--- token-dispenser/programs/token-dispenser/src/tests/test_discord.rs
+++
@@ -1,5 +0,0 @@
-use anchor_lang::prelude::Pubkey;
-use rand::distributions::{Alphanumeric, DistString};
-use crate::{ecosystems::discord::DiscordMessage, Identity, IdentityCertificate};
-
-use super::test_ed25519::Ed25519TestIdentityCertificate;
