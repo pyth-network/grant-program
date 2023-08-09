41423068dcfa2ed060b9eb8b2ac233724cfe49b6
Dev Kalra <dkalra@pyth.network>
2023-08-10 01:18:23 +0530

    comment update

--- frontend/components/wallets/Cosmos.tsx
+++ frontend/components/wallets/Cosmos.tsx
@@ -40,11 +40,11 @@ export function CosmosWalletButton({ chainName }: CosmosWalletButtonProps) {
     disconnect,
   } = chainWalletContext
 
-  // The initial value of `isWalletNotExist` is set to false.
-  // When the user clicks on connect, the value of `isWalletNotExist` changes to false
-  // and then if it the wallet doesnot exist the value `isWalletNotExist` will change to true.
-  // Once, the value of `isWalletNotExist` changes to true, this useEffect will redirect
-  // the user to the keplr webpage.
+  // The initial value of `isWalletNotExist` is false.
+  // When the user clicks on connect, the value of `isWalletNotExist` is first set to false,
+  // doesn't matter what the previous value was. and if it the wallet doesn't exist the
+  // value `isWalletNotExist` will change to true. Once, the value of `isWalletNotExist`
+  // changes to true, this useEffect will redirect the user to the keplr webpage.
   useEffect(() => {
     if (isWalletNotExist) window.open('https://www.keplr.app/download')
   }, [isWalletNotExist])
