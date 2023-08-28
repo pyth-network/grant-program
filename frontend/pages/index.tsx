e1718606755595b29f3a653e14e76a0e1b1dcb71
Robby3082 <32458055+Robby3082@users.noreply.github.com>
2023-08-28 18:42:44 +0300

    change images

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
 import bg from '../images/bg.png'
-import chest from '../images/chest.png'
-import chest2 from '../images/chest2.png'
+import step1 from "../images/step1.png"
+import step2 from "../images/step2.png"
+import step3 from "../images/step3.png"
+import step4 from "../images/step4.png"
+import step5 from "../images/step5.png"
+import step6 from "../images/step6.png"
 
 import Link from 'next/link'
 
@@ -131,11 +135,12 @@ const Claim = () => {
           objectPosition="left bottom"
         />
         <span className="absolute left-0 bottom-0">
-          {step == 6 ? (
-            <Image src={chest2} alt="" />
-          ) : (
-            <Image src={chest} alt="" />
-          )}
+          {step == 1 &&  <Image src={step1} alt="" />}
+          {step == 2 &&  <Image src={step2} alt="" />}
+          {step == 3 &&  <Image src={step3} alt="" />}
+          {step == 4 &&  <Image src={step4} alt="" />}
+          {step == 5 &&  <Image src={step5} alt="" />}
+          {step == 6 &&  <Image src={step6} alt="" />}
         </span>
       </span>
     </>
