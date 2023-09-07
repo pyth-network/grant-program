d4605ba8aa98eb7aba32a12fb82b91efc03db5e9
Guillermo Bescos <g.bescos@yahoo.com>
2023-09-07 11:31:39 +0100

    Add the tests

--- token-dispenser/programs/token-dispenser/src/tests/test_claim.rs
+++ token-dispenser/programs/token-dispenser/src/tests/test_claim.rs
@@ -6,14 +6,23 @@ use {
             copy_keypair,
             IntoTransactionError,
         },
+        ErrorCode,
+        SolanaHasher,
     },
-    anchor_lang::solana_program::program_option::COption,
+    anchor_lang::{
+        prelude::Pubkey,
+        solana_program::program_option::COption,
+    },
     anchor_spl::{
         associated_token::get_associated_token_address,
         token::spl_token::error::TokenError::{
             InsufficientFunds,
             OwnerMismatch,
         },
+    },
+    pythnet_sdk::accumulators::{
+        merkle::MerkleTree,
+        Accumulator,
     },
     solana_program_test::tokio,
     solana_sdk::{
@@ -473,4 +482,145 @@ pub async fn test_claim_fails_if_delegate_revoked() {
         )
         .await
         .unwrap();
+}
+
+#[tokio::test]
+pub async fn test_claim_fails_with_wrong_merkle_proof() {
+    let dispenser_guard: Keypair = Keypair::new();
+
+    let mut simulator = DispenserSimulator::new().await;
+    let claimant_1 = Keypair::new();
+    simulator
+        .airdrop(claimant_1.pubkey(), 1000000000)
+        .await
+        .unwrap();
+
+
+    let (merkle_tree, mock_offchain_certificates_and_claimants) = simulator
+        .initialize_with_claimants(
+            vec![
+                copy_keypair(&simulator.genesis_keypair),
+                copy_keypair(&claimant_1),
+            ],
+            &dispenser_guard,
+        )
+        .await
+        .unwrap();
+
+    let fake_tree_leaf = b"This is a fake tree";
+    let fake_merkle_tree = MerkleTree::<SolanaHasher>::new(&[fake_tree_leaf]).unwrap();
+
+    let claimant_pubkeys = vec![simulator.genesis_keypair.pubkey(), claimant_1.pubkey()];
+
+    for claimant in &claimant_pubkeys {
+        simulator
+            .create_associated_token_account(claimant, &simulator.mint_keypair.pubkey())
+            .await
+            .unwrap();
+    }
+
+    let claim_sums = mock_offchain_certificates_and_claimants
+        .iter()
+        .map(|x| x.1.iter().map(|y| y.amount).sum::<u64>())
+        .collect::<Vec<u64>>();
+
+    let total_claim_sum = claim_sums.iter().sum::<u64>();
+    simulator.mint_to_treasury(total_claim_sum).await.unwrap();
+
+
+    simulator
+        .approve_treasury_delegate(get_config_pda().0, total_claim_sum)
+        .await
+        .unwrap();
+
+    for (claimant, offchain_claim_certificates) in &mock_offchain_certificates_and_claimants {
+        for offchain_claim_certificate in offchain_claim_certificates {
+            let ix_index_error =
+                offchain_claim_certificate.as_instruction_error_index(&merkle_tree);
+            assert_eq!(
+                simulator
+                    .claim(
+                        &copy_keypair(claimant),
+                        offchain_claim_certificate,
+                        &merkle_tree,
+                        None,
+                        Some(fake_merkle_tree.prove(fake_tree_leaf).unwrap()),
+                        None
+                    )
+                    .await
+                    .unwrap_err()
+                    .unwrap(),
+                ErrorCode::InvalidInclusionProof.into_transaction_error(ix_index_error)
+            );
+        }
+    }
+}
+
+#[tokio::test]
+pub async fn test_claim_fails_with_wrong_receipt_pubkey() {
+    let dispenser_guard: Keypair = Keypair::new();
+
+    let mut simulator = DispenserSimulator::new().await;
+    let claimant_1 = Keypair::new();
+    simulator
+        .airdrop(claimant_1.pubkey(), 1000000000)
+        .await
+        .unwrap();
+
+
+    let (merkle_tree, mock_offchain_certificates_and_claimants) = simulator
+        .initialize_with_claimants(
+            vec![
+                copy_keypair(&simulator.genesis_keypair),
+                copy_keypair(&claimant_1),
+            ],
+            &dispenser_guard,
+        )
+        .await
+        .unwrap();
+
+    let claimant_pubkeys = vec![simulator.genesis_keypair.pubkey(), claimant_1.pubkey()];
+
+    for claimant in &claimant_pubkeys {
+        simulator
+            .create_associated_token_account(claimant, &simulator.mint_keypair.pubkey())
+            .await
+            .unwrap();
+    }
+
+    let claim_sums = mock_offchain_certificates_and_claimants
+        .iter()
+        .map(|x| x.1.iter().map(|y| y.amount).sum::<u64>())
+        .collect::<Vec<u64>>();
+
+    let total_claim_sum = claim_sums.iter().sum::<u64>();
+    simulator.mint_to_treasury(total_claim_sum).await.unwrap();
+
+
+    simulator
+        .approve_treasury_delegate(get_config_pda().0, total_claim_sum)
+        .await
+        .unwrap();
+
+    for (claimant, offchain_claim_certificates) in &mock_offchain_certificates_and_claimants {
+        for offchain_claim_certificate in offchain_claim_certificates {
+            let ix_index_error =
+                offchain_claim_certificate.as_instruction_error_index(&merkle_tree);
+            assert_eq!(
+                simulator
+                    .claim(
+                        &copy_keypair(claimant),
+                        offchain_claim_certificate,
+                        &merkle_tree,
+                        None,
+                        None,
+                        Some(Pubkey::new_unique())
+                    )
+                    .await
+                    .unwrap_err()
+                    .unwrap(),
+                ErrorCode::WrongPda.into_transaction_error(ix_index_error)
+            );
+        }
+    }
 }
