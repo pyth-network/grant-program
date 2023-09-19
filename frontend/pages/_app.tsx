9cbdfd510d46d79770d5e9de147568c320dc0bb7
0xfirefist <kalradev@outlook.com>
2023-09-19 19:33:52 +0530

    redirect and clear storage

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AptosWalletProvider } from '@components/wallets/Aptos'
 import { SolanaWalletProvider } from '@components/wallets/Solana'
 import type { AppProps } from 'next/app'
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import { WalletKitProvider as SuiWalletProvider } from '@mysten/wallet-kit'
 
 import { Toaster } from 'react-hot-toast'
@@ -11,8 +11,35 @@ import { SessionProvider } from 'next-auth/react'
 import { EcosystemProviders } from '@components/Ecosystem'
 
 import '../styles/globals.css'
+import { useRouter } from 'next/router'
+import { usePathname } from 'next/navigation'
+
+const LAST_STEP_STATUS_KEY = 'last-step-status-key'
+
+function useRedirect() {
+  const pathname = usePathname()
+  useEffect(() => {
+    localStorage.setItem(LAST_STEP_STATUS_KEY, pathname)
+  }, [pathname])
+
+  const router = useRouter()
+  // We will only redirect on the first load
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+    const lastStep = localStorage.getItem(LAST_STEP_STATUS_KEY)
+    //RULES:
+    // 1. no last state -> redirect to welcome page
+    // 2. there is a last state
+    // 2a. If it is "/next-steps", user has claimed before, redirect to welcome page
+    // 2b. Else redirect to that page.
+    if (lastStep === null) router.replace('/')
+    if (lastStep === '/next-steps') router.replace('/')
+    if (lastStep) router.replace(lastStep)
+  }, [])
+}
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  useRedirect()
   return (
     <SessionProvider>
       <SolanaWalletProvider>
