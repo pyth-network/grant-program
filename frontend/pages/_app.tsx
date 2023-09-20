14d99f6973a11420a5fdbd3c90d27c2c811a5cb8
Dev Kalra <kalradev@outlook.com>
2023-09-20 15:14:44 +0530

    Redirect and clear storage on claim (#131)
    * redirect and clear storage
    
    * switch
    
    * fix
    
    * useEffect only once
    
    * fix redirect on /next-steps

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AptosWalletProvider } from '@components/wallets/Aptos'
 import { SolanaWalletProvider } from '@components/wallets/Solana'
 import type { AppProps } from 'next/app'
-import { FC } from 'react'
+import { FC, useEffect, useMemo } from 'react'
 import { WalletKitProvider as SuiWalletProvider } from '@mysten/wallet-kit'
 
 import { Toaster } from 'react-hot-toast'
@@ -11,8 +11,48 @@ import { SessionProvider } from 'next-auth/react'
 import { EcosystemProviders } from '@components/Ecosystem'
 
 import '../styles/globals.css'
+import { usePathname, useRouter } from 'next/navigation'
+
+const LAST_STEP_STATUS_KEY = 'last-step-status-key'
+
+function useRedirect() {
+  // We are fetching it here and not in useEffect
+  // As we need this before it is being reset
+  const lastStep = useMemo(() => {
+    if (typeof window === 'undefined') return null
+    return localStorage.getItem(LAST_STEP_STATUS_KEY)
+  }, [])
+
+  const router = useRouter()
+  // We will only redirect on the first load
+  useEffect(() => {
+    //RULES:
+    // 1. no last state -> redirect to welcome page
+    // 2. there is a last state
+    // 2a. If it is "/next-steps", user has claimed before, redirect to welcome page
+    // 2b. Else redirect to that page.
+
+    if (lastStep === null) router.replace('/')
+    // NOTE: lastStep will never be /next-steps as we are not storing it in the useEffect
+    // below.
+    // The reason is that the below replace method doesn't work. No clue why.
+    // This is a workaround for now.
+    // if (lastStep === '/next-steps') router.replace('/')
+
+    // lastStep will never be '/next-steps' for other redirect to them
+    if (lastStep) router.replace(lastStep)
+  }, [])
+
+  const pathname = usePathname()
+  useEffect(() => {
+    if (pathname === '/next-steps')
+      localStorage.removeItem(LAST_STEP_STATUS_KEY)
+    else localStorage.setItem(LAST_STEP_STATUS_KEY, pathname)
+  }, [pathname])
+}
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  useRedirect()
   return (
     <SessionProvider>
       <SolanaWalletProvider>
