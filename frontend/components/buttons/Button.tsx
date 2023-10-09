bd99a9038a031e61db22e92cd27368027eb89b19
0xfirefist <kalradev@outlook.com>
2023-10-09 18:27:16 +0530

    tone down proceed

--- frontend/components/buttons/Button.tsx
+++ frontend/components/buttons/Button.tsx
@@ -3,7 +3,7 @@ import { classNames } from 'utils/classNames'
 
 export type ButtonProps = {
   onClick: () => void
-  type: 'primary' | 'secondary' | 'tertiary'
+  type: 'primary' | 'secondary'
   disabled?: boolean
   children: ReactNode
 }
@@ -11,12 +11,15 @@ export type ButtonProps = {
 export function Button({ onClick, type, disabled, children }: ButtonProps) {
   const className =
     type === 'primary'
-      ? 'btn before:btn-bg  btn--light before:bg-light hover:text-light hover:before:bg-dark disabled:text-dark disabled:before:bg-light disabled:opacity-75 disabled:cursor-not-allowed'
-      : type === 'secondary'
-      ? 'btn before:btn-bg  btn--dark before:bg-dark hover:text-dark hover:before:bg-light disabled:text-light disabled:before:bg-dark disabled:opacity-75 disabled:cursor-not-allowed'
-      : 'hover:cursor-pointer hover:font-bold disabled:cursor-not-allowed disabled:opacity-75 '
+      ? 'btn--light before:bg-light hover:text-light hover:before:bg-dark disabled:text-dark disabled:before:bg-light'
+      : 'btn--dark before:bg-dark hover:text-dark hover:before:bg-light disabled:text-light disabled:before:bg-dark'
+
   return (
-    <button className={className} onClick={onClick} disabled={disabled}>
+    <button
+      className={`btn before:btn-bg ${className} disabled:cursor-not-allowed disabled:opacity-40`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <span
         className={
           'relative inline-flex items-center gap-2.5 whitespace-nowrap'
