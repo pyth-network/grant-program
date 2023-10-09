90be5ffbb5e78f69aaeb65449badae7edd3fb20a
Dev Kalra <kalradev@outlook.com>
2023-10-10 02:06:13 +0530

    Tone down proceed button (#179)
    * tone down proceed
    
    * readd tertiary

--- frontend/components/buttons/Button.tsx
+++ frontend/components/buttons/Button.tsx
@@ -11,12 +11,17 @@ export type ButtonProps = {
 export function Button({ onClick, type, disabled, children }: ButtonProps) {
   const className =
     type === 'primary'
-      ? 'btn before:btn-bg  btn--light before:bg-light hover:text-light hover:before:bg-dark disabled:text-dark disabled:before:bg-light disabled:opacity-75 disabled:cursor-not-allowed'
+      ? 'btn before:btn-bg btn--light before:bg-light hover:text-light hover:before:bg-dark disabled:text-dark disabled:before:bg-light'
       : type === 'secondary'
-      ? 'btn before:btn-bg  btn--dark before:bg-dark hover:text-dark hover:before:bg-light disabled:text-light disabled:before:bg-dark disabled:opacity-75 disabled:cursor-not-allowed'
-      : 'hover:cursor-pointer hover:font-bold disabled:cursor-not-allowed disabled:opacity-75 '
+      ? 'btn before:btn-bg btn--dark before:bg-dark hover:text-dark hover:before:bg-light disabled:text-light disabled:before:bg-dark'
+      : 'hover:cursor-pointer hover:font-bold'
+
   return (
-    <button className={className} onClick={onClick} disabled={disabled}>
+    <button
+      className={`${className} disabled:cursor-not-allowed disabled:opacity-40`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <span
         className={
           'relative inline-flex items-center gap-2.5 whitespace-nowrap'
