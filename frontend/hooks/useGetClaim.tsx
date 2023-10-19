2797da3f1954054f5530611bac9079142b782479
0xfirefist <kalradev@outlook.com>
2023-10-19 19:12:40 +0530

    update condition

--- frontend/hooks/useGetClaim.tsx
+++ frontend/hooks/useGetClaim.tsx
@@ -15,7 +15,7 @@ export function useGetClaim() {
       const eligibility = getEligibility(ecosystem)
       const signature = getSignature(ecosystem)
 
-      if (eligibility === undefined) return undefined
+      if (eligibility === undefined || (signature === undefined) && (ecosystem!== Ecosystem.SOLANA )) return undefined
       if (eligibility.isClaimAlreadySubmitted === true) return undefined
 
       return {
