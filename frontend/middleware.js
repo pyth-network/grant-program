28bcc8539cbb277d75b701c9cb86434a96a92eee
Daniel Chew <cctdaniel@outlook.com>
2023-10-27 20:54:50 +0900

    add middleware to block US visitors (#203)
    * add middleware to block US visitors
    
    * remove unused comment
    
    * this should work
    
    * show message along with error
    
    * add comment
    
    * change constant to an array

---
+++ frontend/middleware.js
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+
+// Block visitors from these countries
+const BLOCKED_COUNTRIES = ['US']
+
+// Limit middleware pathname config
+export const config = {
+  matcher: '/',
+}
+
+export function middleware(req) {
+  // Extract country
+  const country = req.geo.country
+
+  if (BLOCKED_COUNTRIES.includes(country)) {
+    // Error 451: Unavailable For Legal Reasons
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/451
+    const body = `
+    <html>
+      <body>
+        <h1>Error 451: Unavailable For Legal Reasons</h1>
+        <p>Access to this site is restricted for users accessing from the United States due to legal or regulatory requirements.</p>
+      </body>
+    </html>
+    `
+
+    // NextResponse object does not have a body property so we use Response instead
+    return new Response(body, {
+      status: 451,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+    })
+  } else {
+    // Continue with the request if the country is not blocked
+    return NextResponse.next()
+  }
+}
