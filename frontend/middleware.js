df19a04040322e3fb84ae620a1f2f8130727221d
Daniel Chew <cctdaniel@outlook.com>
2023-10-27 14:16:53 +0900

    show message along with error

--- frontend/middleware.js
+++ frontend/middleware.js
@@ -15,19 +15,20 @@ export function middleware(req) {
   if (country === BLOCKED_COUNTRY) {
     // Error 451: Unavailable For Legal Reasons
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/451
-    return new NextResponse(null, {
+    const body = `
+    <html>
+      <body>
+        <h1>Error 451: Unavailable For Legal Reasons</h1>
+        <p>Access to this site is restricted for users accessing from the United States due to legal or regulatory requirements.</p>
+      </body>
+    </html>
+    `
+
+    return new Response(body, {
       status: 451,
       headers: {
         'Content-Type': 'text/html',
       },
-      body: `
-          <html>
-            <body>
-              <h1>Error 451: Unavailable For Legal Reasons</h1>
-              <p>Access to this site is restricted for users accessing from the United States due to legal or regulatory requirements.</p>
-            </body>
-          </html>
-        `,
     })
   } else {
     // Continue with the request if the country is not blocked
