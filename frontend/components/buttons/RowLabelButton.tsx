92bf75b27f842e1f0e68b7b85ef24192f85bdeb5
0xfirefist <kalradev@outlook.com>
2023-11-13 19:34:41 +0530

    breakdown button fix - text wasn't wrapping

--- frontend/components/buttons/RowLabelButton.tsx
+++ frontend/components/buttons/RowLabelButton.tsx
@@ -1,5 +1,4 @@
 import Tooltip from '@images/tooltip-purple.inline.svg'
-import { Button } from './Button'
 
 type RowLabelButtonProps = {
   onClick: () => void
@@ -10,12 +9,13 @@ export function RowLabelButton({ onClick, label }: RowLabelButtonProps) {
   const windowWidth = window.innerWidth
   const mobile = windowWidth < 600
   return (
-    <Button onClick={onClick} type={'tertiary'}>
-      <span className="flex items-center gap-2 pr-2 font-header text-base font-semibold leading-none sm:text-base18 sm:font-semibold">
-        {label}
-        {/* We don't have much space on the mobile screen real estate so we have to choose not to show certain contents */}
-        {!mobile ? <Tooltip /> : null}
-      </span>
-    </Button>
+    <span
+      className="flex items-center gap-2 pr-2 font-header text-base font-semibold leading-none hover:cursor-pointer sm:text-base18"
+      onClick={onClick}
+    >
+      {label}
+      {/* We don't have much space on the mobile screen real estate so we have to choose not to show certain contents */}
+      {!mobile ? <Tooltip /> : null}
+    </span>
   )
 }
